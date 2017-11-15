deadb5f5dff535fa0c090825ffc737bd5e4dffeb
Owen Smith <owen@duedil.com>
2017-11-14 23:08:52 -0500

    Add resource stats for macos:
    The timings here are a little cruder than the linux versions, but as with the clock_gettime shim, I'm not sure they're so far from reality as to be problematic.
    
    The I/O stats, however, are much worse. AFAICT macos has no way to get io counters equivalent to what procfs provides. The closest thing I could find would be dtrace probes, which would be ridiculously unwieldy (and I expect costly) for this use case, not to mention requiring root and a one-off SIP reconfig.
    
    I've opted for basic disk io stats from getrusage, based on (hardcoded) block size. Of course, this doesn't include network io, which is a major difference to the linux version.

---
+++ src/spx_resource_stats-macos.c
@@ -0,0 +1,44 @@
+#define _GNU_SOURCE
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include "spx_resource_stats.h"
+#include "spx_thread.h"
+
+void spx_resource_stats_init(void)
+{
+}
+
+void spx_resource_stats_shutdown(void)
+{
+}
+
+
+size_t spx_resource_stats_wall_time(void)
+{
+    struct timeval tv;
+    int ret = 0;
+    ret = gettimeofday(&tv, NULL);
+    if (ret == 0) {
+        return tv.tv_sec * 1000 * 1000 + tv.tv_usec;
+    }
+    return ret;
+}
+
+size_t spx_resource_stats_cpu_time(void)
+{
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru);
+    return (ru.ru_utime.tv_sec  + ru.ru_stime.tv_sec ) * 1000 * 1000 +
+           (ru.ru_utime.tv_usec + ru.ru_stime.tv_usec);
+}
+
+void spx_resource_stats_io(size_t * in, size_t * out)
+{
+    *in = 0;
+    *out = 0;
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru);
+    *in  = ru.ru_inblock * 512;
+    *out = ru.ru_oublock * 512;
+}
--- src/spx_resource_stats.c
+++ src/spx_resource_stats.c
@@ -1,5 +1,7 @@
 #ifdef linux
 #   include "spx_resource_stats-linux.c"
+#elif defined(__APPLE__) && defined(__MACH__)
+#   include "spx_resource_stats-macos.c"
 #else
 #   error "Your platform is not supported. Please open an issue."
 #endif
