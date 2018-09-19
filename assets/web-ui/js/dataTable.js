75be219b190ae4b42616dd64a2b3abd9e81eb439
NoiseByNorthwest <noisebynorthwest@gmail.com>
2018-09-19 23:09:32 +0200

    Change license

--- assets/web-ui/js/dataTable.js
+++ assets/web-ui/js/dataTable.js
@@ -1,3 +1,21 @@
+/* SPX - A simple profiler for PHP
+ * Copyright (C) 2018 Sylvain Lassaut <NoiseByNorthwest@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 
 export function makeDataTable(containerId, options, rows) {
     let sort_col = 0;
