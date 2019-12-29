f8084939957cf5c43815a4c1c081593d6f54cf49
NoiseByNorthwest <noisebynorthwest@gmail.com>
2019-12-29 19:22:23 +0100

    Update licence copyright date

--- src/spx_reporter_fp.c
+++ src/spx_reporter_fp.c
@@ -1,5 +1,5 @@
 /* SPX - A simple profiler for PHP
- * Copyright (C) 2018 Sylvain Lassaut <NoiseByNorthwest@gmail.com>
+ * Copyright (C) 2017-2020 Sylvain Lassaut <NoiseByNorthwest@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
