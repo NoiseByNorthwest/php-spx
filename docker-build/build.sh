e989196deb91626b860bea31579e27a1794a86b1
8ctopus <hello@octopuslabs.io>
2021-07-07 10:55:45 +0500

    updated: alpine build to 3.14.0

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -4,5 +4,5 @@ DOCKER_BUILDKIT=1 docker build --file Dockerfile-ubuntu --build-arg "VERSION=20.
 # build for ubuntu 18.04
 DOCKER_BUILDKIT=1 docker build --file Dockerfile-ubuntu --build-arg "VERSION=18.04" --output type=local,dest=lib .
 
-# build for alpine 3.12.1
-DOCKER_BUILDKIT=1 docker build --file Dockerfile-alpine --build-arg "VERSION=3.12.1" --output type=local,dest=lib .
+# build for alpine 3.14.0
+DOCKER_BUILDKIT=1 docker build --file Dockerfile-alpine --build-arg "VERSION=3.14.0" --output type=local,dest=lib .
