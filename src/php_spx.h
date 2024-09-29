ace5000342fbcc57e0434327112109ad2c188be2
Andy Postnikov <apostnikov@gmail.com>
2024-09-29 10:22:02 +0200

    Declare PHP 8.4 compatibility

--- src/php_spx.h
+++ src/php_spx.h
@@ -31,7 +31,7 @@
 #   error "Only x86-64 and ARM64 architectures are supported"
 #endif
 
-#if ZEND_MODULE_API_NO < 20100525 || ZEND_MODULE_API_NO > 20230831 // 8.3-RC5
+#if ZEND_MODULE_API_NO < 20100525 || ZEND_MODULE_API_NO > 20240924 // 8.4-RC1
 #   error "Only the following PHP versions are supported: 5.4 to 8.3"
 #endif
 
